36c7a713768f2477fcf0ef8eb1e97d42b8c7f7e9
Daniel Schepler <dschepler@gmail.com>
2016-04-15 15:22:16 -0700

    Change NaturalDeduction to use PropLang instead of a private copy

--- NaturalDeduction.v
+++ NaturalDeduction.v
@@ -1,62 +1,53 @@
+Require Import PropLang.
+
 Section natural_deduction.
 
-Variable atom : Type.
+Context {atom : Type}.
 
-Inductive ND_prop : Type :=
-| ND_atom_prop : atom -> ND_prop
-| ND_False : ND_prop
-| ND_True : ND_prop
-| ND_or : ND_prop -> ND_prop -> ND_prop
-| ND_and : ND_prop -> ND_prop -> ND_prop
-| ND_impl : ND_prop -> ND_prop -> ND_prop.
-
-Definition ND_not (P : ND_prop) := ND_impl P ND_False.
+Reserved Notation "Γ ⊢ P" (no associativity, at level 61).
 
 Require Import List.
 
-Inductive ND_proves : list ND_prop -> ND_prop -> Prop :=
-| ND_exfalso_quodlibet {Gamma P} :
-  ND_proves Gamma ND_False ->
-  ND_proves Gamma P
-| ND_True_intro {Gamma} :
-  ND_proves Gamma ND_True
-| ND_or_introl {Gamma P Q} :
-  ND_proves Gamma P ->
-  ND_proves Gamma (ND_or P Q)
-| ND_or_intror {Gamma P Q} :
-  ND_proves Gamma Q ->
-  ND_proves Gamma (ND_or P Q)
-| ND_proof_by_cases {Gamma P Q R} :
-  ND_proves Gamma (ND_or P Q) ->
-  ND_proves (cons P Gamma) R ->
-  ND_proves (cons Q Gamma) R ->
-  ND_proves Gamma R
-| ND_and_intro {Gamma P Q} :
-  ND_proves Gamma P ->
-  ND_proves Gamma Q ->
-  ND_proves Gamma (ND_and P Q)
-| ND_and_elim {Gamma P Q R} :
-  ND_proves Gamma (ND_and P Q) ->
-  ND_proves (cons P (cons Q Gamma)) R ->
-  ND_proves Gamma R
-| ND_cond_proof {Gamma P Q} :
-  ND_proves (cons P Gamma) Q ->
-  ND_proves Gamma (ND_impl P Q)
-| ND_modus_ponens {Gamma P Q} :
-  ND_proves Gamma (ND_impl P Q) ->
-  ND_proves Gamma P ->
-  ND_proves Gamma Q
-| ND_assumption {Gamma P} :
-  In P Gamma ->
-  ND_proves Gamma P
-| ND_sequence {Gamma P Q} :
-  ND_proves Gamma P ->
-  ND_proves (cons P Gamma) Q ->
-  ND_proves Gamma Q
-(* | ND_proof_by_contradiction {Gamma P} :
-  ND_proves (cons (ND_not P) Gamma) ND_False ->
-  ND_proves Gamma P *)
-.
+Inductive ND_proves : list (prop atom) -> prop atom -> Prop :=
+| ND_exfalso_quodlibet {Γ P} :
+  Γ ⊢ ⊥ ->
+  Γ ⊢ P
+| ND_True_intro {Γ} :
+  Γ ⊢ ⊤
+| ND_or_introl {Γ P Q} :
+  Γ ⊢ P ->
+  Γ ⊢ P ∨ Q
+| ND_or_intror {Γ P Q} :
+  Γ ⊢ Q ->
+  Γ ⊢ P ∨ Q
+| ND_proof_by_cases {Γ P Q R} :
+  Γ ⊢ P ∨ Q ->
+  P :: Γ ⊢ R ->
+  Q :: Γ ⊢ R ->
+  Γ ⊢ R
+| ND_and_intro {Γ P Q} :
+  Γ ⊢ P ->
+  Γ ⊢ Q ->
+  Γ ⊢ P ∧ Q
+| ND_and_elim {Γ P Q R} :
+  Γ ⊢ P ∧ Q ->
+  P :: Q :: Γ ⊢ R ->
+  Γ ⊢ R
+| ND_cond_proof {Γ P Q} :
+  P :: Γ ⊢ Q ->
+  Γ ⊢ P ⊃ Q
+| ND_modus_ponens {Γ P Q} :
+  Γ ⊢ P ⊃ Q ->
+  Γ ⊢ P ->
+  Γ ⊢ Q
+| ND_assumption {Γ P} :
+  In P Γ ->
+  Γ ⊢ P
+| ND_cut {Γ P Q} :
+  Γ ⊢ P ->
+  P :: Γ ⊢ Q ->
+  Γ ⊢ Q
+where "Γ ⊢ P" := (ND_proves Γ P).
 
 Lemma ND_context_extension {Gamma1 Gamma2 P} :
 Forall (fun x => In x Gamma2) Gamma1 -> ND_proves Gamma1 P ->
@@ -86,17 +77,17 @@ intros. revert Gamma2 H. induction H0; intros.
   - apply Forall_impl with (2 := H). intros. right. assumption.
 + apply (ND_modus_ponens (P := P)); auto.
 + apply ND_assumption. rewrite Forall_forall in H0. auto.
-+ apply (ND_sequence (P := P)); auto.
++ apply (ND_cut (P := P)); auto.
   apply IHND_proves2. constructor.
   - left. reflexivity.
   - apply Forall_impl with (2 := H). intros. right. assumption.
 Qed.
 
 (* Want to prove: ND_prop forms a Heyting algebra *)
-Definition ND_prop_le (P Q:ND_prop) : Prop :=
+Definition ND_prop_le (P Q:prop atom) : Prop :=
 ND_proves (cons P nil) Q.
 
-Definition ND_prop_eqv (P Q:ND_prop) : Prop :=
+Definition ND_prop_eqv (P Q:prop atom) : Prop :=
 ND_prop_le P Q /\ ND_prop_le Q P.
 
 Lemma ND_prop_le_refl {P} : ND_prop_le P P.
@@ -107,21 +98,21 @@ Qed.
 Lemma ND_prop_le_trans {P Q R} :
 ND_prop_le P Q -> ND_prop_le Q R -> ND_prop_le P R.
 Proof.
-intros. apply (ND_sequence (P := Q)).
+intros. apply (ND_cut (P := Q)).
 + assumption.
 + apply ND_context_extension with (2 := H0). constructor.
   - left. reflexivity.
   - constructor.
 Qed.
 
-Lemma ND_meet1 {P Q} : ND_prop_le (ND_and P Q) P.
+Lemma ND_meet1 {P Q} : ND_prop_le (P ∧ Q) P.
 Proof.
 apply (ND_and_elim (P := P) (Q := Q)).
 + apply ND_assumption. left. reflexivity.
 + apply ND_assumption. left. reflexivity.
 Qed.
 
-Lemma ND_meet2 {P Q} : ND_prop_le (ND_and P Q) Q.
+Lemma ND_meet2 {P Q} : ND_prop_le (P ∧ Q) Q.
 Proof.
 apply (ND_and_elim (P := P) (Q := Q)).
 + apply ND_assumption. left. reflexivity.
@@ -129,25 +120,25 @@ apply (ND_and_elim (P := P) (Q := Q)).
 Qed.
 
 Lemma ND_meet_max {P Q R} : ND_prop_le R P -> ND_prop_le R Q ->
-  ND_prop_le R (ND_and P Q).
+  ND_prop_le R (P ∧ Q).
 Proof.
 intros. apply ND_and_intro.
 + assumption.
 + assumption.
 Qed.
 
-Lemma ND_join1 {P Q} : ND_prop_le P (ND_or P Q).
+Lemma ND_join1 {P Q} : ND_prop_le P (P ∨ Q).
 Proof.
 apply ND_or_introl. apply ND_assumption. left. reflexivity.
 Qed.
 
-Lemma ND_join2 {P Q} : ND_prop_le Q (ND_or P Q).
+Lemma ND_join2 {P Q} : ND_prop_le Q (P ∨ Q).
 Proof.
 apply ND_or_intror. apply ND_assumption. left. reflexivity.
 Qed.
 
 Lemma ND_join_min {P Q R} : ND_prop_le P R -> ND_prop_le Q R ->
-ND_prop_le (ND_or P Q) R.
+ND_prop_le (P ∨ Q) R.
 Proof.
 intros. apply (ND_proof_by_cases (P := P) (Q := Q)).
 + apply ND_assumption. left. reflexivity.
@@ -159,19 +150,19 @@ intros. apply (ND_proof_by_cases (P := P) (Q := Q)).
   - constructor.
 Qed.
 
-Lemma ND_False_min {P} : ND_prop_le ND_False P.
+Lemma ND_False_min {P} : ND_prop_le ⊥ P.
 Proof.
 apply ND_exfalso_quodlibet. apply ND_assumption.
 left. reflexivity.
 Qed.
 
-Lemma ND_True_max {P} : ND_prop_le P ND_True.
+Lemma ND_True_max {P} : ND_prop_le P ⊤.
 Proof.
 apply ND_True_intro.
 Qed.
 
 Lemma ND_Heyting_algebra {P Q R} :
-  ND_prop_le P (ND_impl Q R) <-> ND_prop_le (ND_and P Q) R.
+  ND_prop_le P (Q ⊃ R) <-> ND_prop_le (P ∧ Q) R.
 Proof.
 split; intros.
 + apply (ND_and_elim (P := P) (Q := Q)).
@@ -181,7 +172,7 @@ split; intros.
       { left. reflexivity. }
       { constructor. }
     * apply ND_assumption. right. left. reflexivity.
-+ apply ND_cond_proof. apply (ND_sequence (P := ND_and P Q)).
++ apply ND_cond_proof. apply (ND_cut (P := P ∧ Q)).
   - apply ND_and_intro.
     * apply ND_assumption. right. left. reflexivity.
     * apply ND_assumption. left. reflexivity.
@@ -210,18 +201,18 @@ Hypotheses
   (join_min : forall x y z:X, le x z -> le y z -> le (join x y) z)
   (Heyting_cond : forall x y z:X, le x (arrow y z) <-> le (meet x y) z).
 
-Fixpoint F (P : ND_prop) : X :=
+Fixpoint F (P : prop atom) : X :=
 match P with
-| ND_atom_prop a => f a
-| ND_False => bot
-| ND_True => top
-| ND_or Q R => join (F Q) (F R)
-| ND_and Q R => meet (F Q) (F R)
-| ND_impl Q R => arrow (F Q) (F R)
+| atom_prop a => f a
+| ⊥ => bot
+| ⊤ => top
+| Q ∨ R => join (F Q) (F R)
+| Q ∧ R => meet (F Q) (F R)
+| Q ⊃ R => arrow (F Q) (F R)
 end.
 
-Lemma proves_cond : forall Gamma P, ND_proves Gamma P ->
-  le (F (fold_right ND_and ND_True Gamma)) (F P).
+Lemma proves_cond {Γ P} : Γ ⊢ P ->
+  le (F (fold_right and_prop ⊤ Γ)) (F P).
 Proof.
 induction 1; simpl in * |-*.
 + apply le_trans with (1 := IHND_proves). apply bot_min.
@@ -241,11 +232,11 @@ induction 1; simpl in * |-*.
 + rewrite Heyting_cond in IHND_proves1. rewrite <- Heyting_cond in IHND_proves1.
   pose proof (meet_max _ _ _ IHND_proves1 IHND_proves2).
   apply le_trans with (1 := H1). rewrite <- Heyting_cond. apply le_refl.
-+ induction Gamma; simpl.
++ induction Γ; simpl.
   - destruct H.
   - destruct H.
     * rewrite <- H. apply meet_left.
-    * apply le_trans with (2 := IHGamma H). apply meet_right.
+    * apply le_trans with (2 := IHΓ H). apply meet_right.
 + apply le_trans with (2 := IHND_proves2). apply meet_max.
   - assumption.
   - apply le_refl.
@@ -253,7 +244,7 @@ Qed.
 
 Corollary F_incr : forall P Q, ND_prop_le P Q -> le (F P) (F Q).
 Proof.
-intros. apply le_trans with (2 := proves_cond _ _ H). simpl.
+intros. apply le_trans with (2 := proves_cond H). simpl.
 apply meet_max.
 + apply le_refl.
 + apply top_max.
